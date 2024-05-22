83d477786020097b50929216b917fdc2a9ce3dad
Tararaz <aurelius.wijaya@binus.ac.id>
2024-05-22 19:52:12 +0700

    api endpoint

--- server/main.go
+++ server/main.go
@@ -38,3 +38,10 @@ func main() {
 
 	router.Run(":" + port)
 }
+echo "SE FUIYOO" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Tararaz/SE.git
+git push -u origin main
